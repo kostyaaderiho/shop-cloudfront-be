30895eab00cfe42ccf2309905b9c3d5a2e9b230f
Kanstantsin Adzerykha <kanstantsin_adzerykha@epam.com>
2022-07-28 13:17:48 +0300

    Updated cors headers

--- product-service/src/constants/cors.ts
+++ product-service/src/constants/cors.ts
@@ -1,3 +1,6 @@
 export const CORS_HEADERS = {
-    'Access-Control-Allow-Origin': '*'
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': '*',
+    'Access-Control-Allow-Credentials': true,
+    'Access-Control-Allow-Methods': 'GET, OPTIONS, POST'
 };
