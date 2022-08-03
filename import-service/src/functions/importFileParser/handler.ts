fee2065219244ef386e430ac814d3f9c0608d5e0
Kanstantsin Adzerykha <kanstantsin_adzerykha@epam.com>
2022-08-03 19:57:45 +0300

    Refactoring

--- import-service/src/functions/importFileParser/handler.ts
+++ import-service/src/functions/importFileParser/handler.ts
@@ -1,55 +1,34 @@
-import { middyfy } from '@libs/lambda';
-import AWS from 'aws-sdk';
-import csv from 'csv-parser';
-
+import { parseCSVFile, CopyFile, DeleteFile } from '../../utils';
+import { middyfy } from '../../libs';
 import {
     PRODUCT_CSV_FILES_BUCKET_UPLOADED_FOLDER,
     PRODUCT_CSV_FILES_BUCKET_PARSED_FOLDER,
-    PRODUCT_CSV_FILES_BUCKET_REGION,
     CORS_HEADERS
 } from '../../constants';
 
-export const importFileParser = async (event) => {
-    const S3 = new AWS.S3({
-        region: PRODUCT_CSV_FILES_BUCKET_REGION
-    });
-
-    let PARAMS;
-
+export const importFileParser = async (event: {
+    Records: { s3: { bucket: { name: string }; object: { key: string } } }[];
+}) => {
     for (const record of event.Records) {
-        PARAMS = {
+        const PARAMS = {
             Bucket: record.s3.bucket.name,
             Key: decodeURIComponent(record.s3.object.key)
         };
+
+        const records = await parseCSVFile(PARAMS);
+
+        console.log('records', records);
+
+        const copiedFile = await CopyFile({
+            from: PRODUCT_CSV_FILES_BUCKET_UPLOADED_FOLDER,
+            to: PRODUCT_CSV_FILES_BUCKET_PARSED_FOLDER,
+            params: PARAMS
+        });
+
+        if (copiedFile) {
+            await DeleteFile({ params: PARAMS });
+        }
     }
-
-    const s3ObjectStream = S3.getObject(PARAMS).createReadStream();
-
-    await new Promise((resolve, reject) => {
-        let results = [];
-        s3ObjectStream
-            .pipe(csv())
-            .on('data', (data) => results.push(data))
-            .on('error', (err) => reject(err))
-            .on('end', () => {
-                console.log(results);
-                resolve(results);
-            });
-    });
-
-    await S3.copyObject({
-        CopySource: PARAMS.Bucket + '/' + PARAMS.Key,
-        Key: PARAMS.Key.replace(
-            PRODUCT_CSV_FILES_BUCKET_UPLOADED_FOLDER,
-            PRODUCT_CSV_FILES_BUCKET_PARSED_FOLDER
-        ),
-        Bucket: PARAMS.Bucket
-    }).promise();
-
-    await S3.deleteObject({
-        Bucket: PARAMS.Bucket,
-        Key: PARAMS.Key
-    }).promise();
 
     return {
         statusCode: 202,
